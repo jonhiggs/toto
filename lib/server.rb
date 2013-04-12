1a882176d745267e83fdaa91da1bd89b342149b4
Jon Higgs <jhiggs0@gmail.com>
2013-04-12 21:06:10 +0200

    break up toto.rb into something a little less unwieldly.

---
+++ lib/server.rb
@@ -0,0 +1,39 @@
+module Toto
+  class Server
+    attr_reader :config, :site
+
+    def initialize config = {}, &blk
+      @config = config.is_a?(Config) ? config : Config.new(config)
+      @config.instance_eval(&blk) if block_given?
+      @site = Toto::Site.new(@config)
+    end
+
+    def call env
+      @request  = Rack::Request.new env
+      @response = Rack::Response.new
+
+      return [400, {}, []] unless @request.get?
+
+      path, mime = @request.path_info.split('.')
+      route = (path || '/').split('/').reject {|i| i.empty? }
+
+      response = @site.go(route, env, *(mime ? mime : []))
+
+      @response.body = [response[:body]]
+      @response['Content-Length'] = response[:body].bytesize.to_s unless response[:body].empty?
+      @response['Content-Type']   = Rack::Mime.mime_type(".#{response[:type]}")
+
+      # Set http cache headers
+      @response['Cache-Control'] = if Toto.env == 'production'
+        "public, max-age=#{@config[:cache]}"
+      else
+        "no-cache, must-revalidate"
+      end
+
+      @response['ETag'] = %("#{Digest::SHA1.hexdigest(response[:body])}")
+
+      @response.status = response[:status]
+      @response.finish
+    end
+  end
+end
